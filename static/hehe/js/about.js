815eeb89759bcb9c1a47e8ad5cf5ab2373c3f63c
wpfAIKrj <18516894379@1163.com>
2017-12-12 14:30:59 +0800

    fix：关于我们接口的对接

--- static/hehe/js/about.js
+++ static/hehe/js/about.js
@@ -1,20 +1,21 @@
-//page data
-$(document).ready(function(){
-	
-	var urlinfo=window.location.href; //获取当前页面的url 
-	var newsid=getURLParameter('nid');//得到参数值 
-	getNewsDetail(newsid); 
-	
+
+new Vue({
+	el:'#news-detail',
+	data:{
+		newsdetail:"",
+	},
+	created: function() {
+		this.getAboutDetail(); 
+	},
+	methods:{
+		getAboutDetail(){
+			axios.get(`http://staging.hehefilm.com/resources/about_me`)
+			.then(resp => {
+				this.newsdetail = resp.data.adetail;
+				console.log(resp.data.adetail);
+			}).catch(err => {
+				console.log('请求失败：'+err.status+','+err.statusText);
+			});
+		},
+	},
 });
-
-function getURLParameter(name) {
-    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
-}
-
-function getNewsDetail(newsid) {
-	$.get("https://shanxiaxuetang.com/resources/news/" + newsid,function(rdata, status){
-		var jsonobj = $.parseJSON(rdata);
-		$('#news-detail').prepend(jsonobj.cnt);
-		
-	});
-}
